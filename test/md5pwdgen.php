61702e759f3532aa8012ad409032731fe053d693
Borisov <s_os@inbox.ru>
2016-09-13 11:31:16 +0300

    Groups added

---
+++ test/md5pwdgen.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ABorisov
+ * Date: 13.09.2016
+ * Time: 10:10
+ */
+$db['dbhost']="localhost";
+$db['dbname']="bdata";
+$db['dbuser']="root";
+$db['dbpassword']="mysql";
+/*
+$word['langth']=1;
+$word['data']=array();
+$word['alfabet']=array('1','2','3','4','5','6','7','8','9','0','!','@','#','$','^','&','*','(',')','-','=','_','+',
+    '[',']','{','}','\"','№',';','%',':','?','\\','|','/','.',',',
+    'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e',
+    'r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m');
+$mass = sizeof($word['alfabet']);
+
+#for ($num=0; $num <=1; $num++ ) {
+  #  for ($in_num=0; $in_num >= $mass;$in_num++){
+  #      $word['pass'][$in_num]=$word[$in_num];
+
+#echo "1";
+   # }
+
+#}
+#print_r($word);
+#$word['']='';
+#$word['']='';
+#$word['']='';
+#$word['']='';
+#$word['']='';
+#$word['']='';
+
+
+
